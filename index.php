a566289debd9c991be10cc7d32312422511da277
Binapick <asteiner@stoneship.at>
2016-06-26 18:04:01 +0200

    contact form, proclem with the select a file button

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
   belongs to : Maria DB
   contact    : Andreas Steiner   (astone@stoneship.at)
   copyright 2016
-  <!-- phpDesigner :: Timestamp [26.06.2016 12:52:28] -->
+  <!-- phpDesigner :: Timestamp [26.06.2016 17:09:53] -->
   ------HoC------
  ----------------------------------------------------*/
 ini_set("display_errors",1);
 error_reporting(E_ALL|E_STRICT);
 (isset($_GET["l"]))? $language = $_GET["l"] : $language = "en";
+(isset($_GET["mo"]))? $mo = $_GET["mo"] : $mo = "con";
 
 include_once("./assets/lib/fu_html.inc");
 include_once("./assets/lib/".$language."_lang.inc");
@@ -44,31 +45,9 @@ $o .= " </div>\n";
 
 $o .= getRevolutionSlider();
 
-$o .= "			<!-- CALLOUT -->
-			<div class=\"alert alert-transparent bordered-bottom\">
-				<div class=\"container\">
-
-					<div class=\"row\">
-
-						<div class=\"col-md-9 col-sm-12\"><!-- left text -->
-							<h3>Call now at <span><strong>+800-565-2390</strong></span> and get 15% discount!</h3>
-							<p class=\"font-lato weight-300 size-20 nomargin-bottom\">
-								We truly care about our users and our product.
-							</p>
-						</div><!-- /left text -->
-
-						
-						<div class=\"col-md-3 col-sm-12 text-right\"><!-- right btn -->
-							<a href=\"#purchase\" rel=\"nofollow\" target=\"_blank\" class=\"btn btn-primary btn-lg\">PURCHASE NOW</a>
-						</div><!-- /right btn -->
-
-					</div>
-
-				</div>
-			</div>
-			<!-- /CALLOUT -->";
 
 
+if($mo == "con"){ $o .= include("/frontend/mo_contact.inc"); }
 
 
 
